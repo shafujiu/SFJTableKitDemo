6e629f194963927c3b02608063d7975c783b2ed7
shafujiu <zhaowei200711@163.com>
2021-01-20 16:22:53 +0800

    Initial Commit

---
+++ SFJTableKitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SFJTableKitDemo
+//
+//  Created by Shafujiu on 2021/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
